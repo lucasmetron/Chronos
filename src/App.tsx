466d275c2aab287903df15fc5c3fe3e2c871f079
Lucas Rosa <lucasmetron@gmail.com>
2025-07-08 11:59:49 -0300

    feat: adicionar componente Countdown e substituir Heading por Countdown na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Container from './components/Container';
 import Heading from './components/Heading';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
+import Countdown from './components/Countdown';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
       </Container>
 
       <Container>
-        <Heading>Form</Heading>
+        <Countdown />
       </Container>
 
       <Container>
---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const Countdown = () => {
+  return <div className={styles.container}>00:00</div>;
+};
+
+export default Countdown;
