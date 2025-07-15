e87890ff63b80104ad7e0791f1feea796233b0ed
Lucas Rosa <lucasmetron@gmail.com>
2025-07-15 10:17:48 -0300

    feat: implementar tema claro e escuro no componente Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,25 +1,74 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useLayoutEffect, useState } from 'react';
+
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 
+type ThemeProps = 'dark' | 'light';
+
 const Menu = () => {
+  const themeLocalStorage = 'theme';
+  const [theme, setTheme] = useState<ThemeProps>(() => {
+    const theme =
+      (localStorage.getItem(themeLocalStorage) as ThemeProps) || 'dark';
+    return theme === null ? 'dark' : theme;
+  });
+
+  function handleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
+  }
+
+  useLayoutEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem(themeLocalStorage, theme);
+  }, [theme]);
+
   return (
-    <div className={styles.menu}>
-      <a className={styles.linkMenu}>
+    <nav className={styles.menu}>
+      <a
+        className={styles.linkMenu}
+        aria-label='Ir para a home'
+        title='Ir para a home'
+      >
         <HouseIcon className={styles.icon} />
       </a>
 
-      <a className={styles.linkMenu}>
+      <a
+        className={styles.linkMenu}
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon className={styles.icon} />
       </a>
 
-      <a className={styles.linkMenu}>
+      <a
+        className={styles.linkMenu}
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon className={styles.icon} />
       </a>
 
-      <a className={styles.linkMenu}>
-        <SunIcon className={styles.icon} />
+      <a
+        className={styles.linkMenu}
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        href='#'
+        onClick={event => handleTheme(event)}
+      >
+        {theme === 'dark' ? (
+          <SunIcon className={styles.icon} />
+        ) : (
+          <MoonIcon className={styles.icon} />
+        )}
       </a>
-    </div>
+    </nav>
   );
 };
 
