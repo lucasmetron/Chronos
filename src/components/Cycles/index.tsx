fb1c17d3208539731807edcf6c2e249c33d25280
Lucas Rosa <lucasmetron@gmail.com>
2025-07-17 11:03:46 -0300

    feat: refatorar componente Cycles para gerar ciclos dinamicamente

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,39 @@
+import type { JSX } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 
 const Cycles = () => {
+  const { state } = useTaskContext();
+  const { currentCycle } = state;
+
+  function returnCssCycle(i: number) {
+    if (i === 7) {
+      return styles.longBreakTime;
+    }
+
+    if (i % 2 === 0) {
+      return styles.shortBreakTime;
+    } else {
+      return styles.worktime;
+    }
+  }
+
+  function returnCycles() {
+    const elements: JSX.Element[] = [];
+
+    for (let i = 0; i < currentCycle; i++) {
+      elements.push(
+        <span key={i} className={`${styles.cycleDot} ${returnCssCycle(i)}`} />,
+      );
+    }
+
+    return elements;
+  }
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
-      <div className={styles.cycleDots}>
-        <span className={`${styles.cycleDot} ${styles.worktime}`} />
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`} />
-        <span className={`${styles.cycleDot} ${styles.worktime}`} />
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`} />
-        <span className={`${styles.cycleDot} ${styles.worktime}`} />
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`} />
-        <span className={`${styles.cycleDot} ${styles.worktime}`} />
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`} />
-      </div>
+      <div className={styles.cycleDots}>{returnCycles()}</div>
     </div>
   );
 };
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import { PlayCircleIcon } from 'lucide-react';
 
 import styles from './styles.module.css';
 import DefaultInput from '../DefaultInput';
 import Cycles from '../Cycles';
 import DefaultButton from '../DefaultButton';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 const MainForm = () => {
+  const { setState } = useTaskContext();
+  const [taskName, setTaskNAme] = useState('');
+
+  function createNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setState(prevState => ({
+      ...prevState,
+      currentCycle:
+        prevState.currentCycle === 8 ? 0 : prevState.currentCycle + 1,
+    }));
+  }
   return (
-    <form className={styles.form} action=''>
+    <form onSubmit={createNewTask} className={styles.form} action=''>
       <div className={styles.formRow}>
         <DefaultInput
           id='task'
           type='text'
           labelText='Task'
           placeholder='Digite algo'
+          value={taskName}
+          onChange={e => setTaskNAme(e.target.value)}
         />
       </div>
 
