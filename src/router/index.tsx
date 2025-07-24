1c4ce22d9838a0a704eae82e5e72db93fb723df6
Lucas Rosa <lucasmetron@gmail.com>
2025-07-24 11:36:53 -0300

    feat: substituir RouterProvider por BrowserRouter e implementar ScrollTop para rolagem suave

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,6 @@
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { useLayoutEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+
 import Home from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
 import { AboutPomodoro } from '../pages/AboutPomodoro';
@@ -6,31 +8,30 @@ import Settings from '../pages/Settings';
 import History from '../pages/History';
 import { routes } from './routes';
 
-const router = createBrowserRouter([
-  {
-    path: routes.home,
-    element: <Home />,
-  },
-  {
-    path: routes.history,
-    element: <History />,
-  },
-  {
-    path: routes.settings,
-    element: <Settings />,
-  },
-  {
-    path: routes.about,
-    element: <AboutPomodoro />,
-  },
-  {
-    path: routes.notFound,
-    element: <NotFound />,
-  },
-]);
+const ScrollTop = () => {
+  //hacker para esse componente ser filho de BrowserRouter
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+  return null;
+};
 
 const Router = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.history} element={<History />} />
+        <Route path={routes.settings} element={<Settings />} />
+        <Route path={routes.about} element={<AboutPomodoro />} />
+        <Route path={routes.notFound} element={<NotFound />} />
+      </Routes>
+
+      <ScrollTop />
+    </BrowserRouter>
+  );
 };
 
 export default Router;
